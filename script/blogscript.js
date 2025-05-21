4046fa8302565148fb158426a3a650012d807c25
Neo <danfsiqueira@gmail.com>
2025-05-21 09:24:09 -0300

    bug fix

--- script/blogscript.js
+++ script/blogscript.js
@@ -1,6 +1,6 @@
 const librewolflike = document.getElementById("librewolflikebutton");
-const librewolfhide = document.getElementById("librewolfhideshowbutton")
-const librewolftext = document.getElementById("librewolftext")
+const librewolfhide = document.getElementById("librewolfhideshowbutton"); // fixed typo
+const librewolftext = document.getElementById("librewolftext");
 //librewolf hide button
 librewolfhide.addEventListener("click", function() {
     if (librewolftext.style.display === "block") {
